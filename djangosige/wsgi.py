0cd4ee556816004860f95b653f3d1ee9258f6eca
JacksonOsvaldo <jacksonosvaldo@live.com>
2019-08-30 15:51:22 -0300

    Update 02

--- djangosige/wsgi.py
+++ djangosige/wsgi.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 import sys
-from dj_static import Cling
 
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), "../../")))
@@ -20,4 +19,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosige.configs.settings")
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
